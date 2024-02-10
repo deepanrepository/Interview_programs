6fa03480e2c2670e04bd78c876607dba7a1f37f6
DEEPAN <DEEPAN@LAPTOP-4FQQS340>
2024-02-10 09:20:52 +0530

    add factoriyal final

---
+++ Interview_Program/src/Factorial.java
@@ -0,0 +1,13 @@
+
+public class Factorial {
+	public static void main(String[] args) {
+		int n=5;
+		int fac=1;
+		for (int i = 1; i<=n; i++) {
+			fac*=i;
+			System.out.println(fac);
+		}
+		System.out.println("total fac = "+fac);
+	}
+
+}
